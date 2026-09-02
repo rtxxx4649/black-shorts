3d8a68b25e4f70a756d880887dface845b608fba
rtxxx4649 <rtwww000@gmail.com>
2026-09-03 03:31:28 +0900

    Remove Cloudflare-specific build script

--- scripts/prepare-cloudflare.js
+++
@@ -1,5 +0,0 @@
-import { mkdir, copyFile } from "node:fs/promises";
-
-await mkdir("dist", { recursive: true });
-await copyFile("index2.html", "dist/index.html");
-await copyFile("e2e1003b-f8c4-4441-924c-df430cc634a5.png", "dist/e2e1003b-f8c4-4441-924c-df430cc634a5.png");
